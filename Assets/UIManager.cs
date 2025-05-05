f781afd53344a07d106876d76a43547980f2ff6d
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2025-05-05 09:04:00 -0300

    Textosaludo se borra al iniciar

---
+++ Assets/UIManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI txtSaludo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txtSaludo.text = string.Empty;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
